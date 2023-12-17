8c58447439a2aaded20ca7642fbd92b289a41add
Aleksey-Dr <aprel181818@gmail.com>
2023-12-17 20:00:50 +0200

    fix #11

--- src/favorites.js
+++ src/favorites.js
@@ -4,7 +4,5 @@ import './js/mobileMenu';
 import './js/totopbutton.js';
 import './js/quote.js';
 import './js/subscribe.js';
-import './js/home.js';
 import './js/hero.js';
 import './js/footer.js';
-import './js/modal.js';
