bb60c646a6746ea909d1875f41a71134512790e6
Aleksey-Dr <aprel181818@gmail.com>
2023-12-17 20:12:53 +0200

    fix #11

--- src/favorites.js
+++ src/favorites.js
@@ -1,8 +1,2 @@
 import './js/favorites.js';
-
-import './js/mobileMenu';
-import './js/totopbutton.js';
 import './js/quote.js';
-import './js/subscribe.js';
-import './js/hero.js';
-import './js/footer.js';
