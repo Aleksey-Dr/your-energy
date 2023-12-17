537746f90263565d40d52eaab50e1097657df88d
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2023-12-17 17:58:45 +0000

    Deploying to gh-pages from @ Aleksey-Dr/your-energy@1ca08af2c6a3915c96b42e18b732f09041c228e4 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import{r as e}from"./assets/modal-ca28a2c7.js";import"./assets/vendor-5014e6d0.js";e.exerciseRefresh.addEventListener("click",r);function r(o){location.reload()}
+//# sourceMappingURL=commonHelpers2.js.map
